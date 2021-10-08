ddc1d4b2ef731cbe74f9066fd8a05377e85f55fe
User <User@DESKTOP-PUEKB8G>
2021-10-08 13:43:56 +0100

    Kata 2 version 1 limpia

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -23,63 +23,6 @@ public class Kata2 {
             System.out.println(key + "==>" + histogram.get(key));
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /**
-        Metodo 1
-        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
-            System.out.println("La clave es: " + entry.getKey() + " y el valor es: " + entry.getValue());
-        }
-        
-        //Metodo 2
-        for (Integer integer : hashMap.keySet()) {
-            System.out.println("Hay una clave: " + integer);
-        }
-        
-        for (Integer value : hashMap.values()) {
-            System.out.println("Hay un valor: " + value);
-        }
-        
-        //Metodo 3.A
-        Iterator<Map.Entry<Integer,Integer>> entries = hashMap.entrySet().iterator();
-        while(entries.hasNext()){
-            Map.Entry<Integer, Integer> entry = entries.next();
-            System.out.println("La clave es: " + entry.getKey() + " y el valor es: " + entry.getValue());
-        }
-        
-        //Metodo 3.B
-        Iterator entries2 = hashMap.entrySet().iterator();
-        while(entries2.hasNext()){
-            Map.Entry entry = (Map.Entry) entries2.next();
-            Integer key = (Integer) entry.getKey();
-            Integer value = (Integer) entry.getValue();
-            System.out.println("La clave es: " + entry.getKey() + " y el valor es: " + entry.getValue());
-        }
-        
-        //Metodo 4 (La peor forma)
-        for (Integer key : hashMap.keySet()) {
-            Integer value = hashMap.get(key);
-            System.out.println("La clave es: " + key + " y el valor es: " + value);
-            
-        }
-        **/
-        
     }
     
 }
